f30753861423c7e1602d6e238518f3e9ec4337ef
fatma <fatma@FATMA-PC>
2024-10-10 15:15:20 +0300

    Add project files.

---
+++ 0.5-Döngülerle Pratik Uygulamalar, Yıldızlarla Şekil Çizme/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._5_Döngülerle_Pratik_Uygulamalar__Yıldızlarla_Şekil_Çizme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 10 Tane Yıldız Alt Alta Yazma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+
+            #endregion
+
+            #region 10 Tane Yıldız Yan Yana Yazma
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+
+            #endregion
+
+            #region Dik Üçgen
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for(int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Ters Dik Üçgen
+
+            //for (int i = 5; i >= 1; i--)
+            //{
+            //    for (int j=1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Dik ve Ters Dik Üçgen
+
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for (int k = 4; k >= 1; k--)
+            //{
+            //    for (int l = 1; l <= k; l++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Yarım Baklava Dilimi
+
+            //int n = 5;
+            //// üst kısım
+
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //// alt kısım
+            //for (int i = n - 1; i >= 1; i--)
+            //{
+            //    for (int j = n - 1; j > 0; j--)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 1; k <= 2 * i - 1; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
+
+            #region Piramit
+            int n = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = n - i; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = n - 1; i >= 1; i--)
+            {
+                for (int j = n - i; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 2 * i - 1; k >= 1; k--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
